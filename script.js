c8a31a829bd2a553e88adb7e5c6428876403e8fa
Guilherme Soares <165791068+GuiiSoaress@users.noreply.github.com>
2024-08-06 09:17:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function calculate() {
+    
+    var num11 = (document.getElementById('num1').value);
+    var num22 = (document.getElementById('num2').value);
+
+    var soma =  num11 + num22;
+    document.getElementById('result').textContent = soma;
+
+}
